25ea306ff47daa903f4d59c1e42ce63bc1d0cec8
knighthat <knightchanelgaming@gmail.com>
2024-02-22 21:19:37 -0600

    added 'arguments' for more configurations

---
+++ arguments.py
@@ -0,0 +1,16 @@
+from multiprocessing import cpu_count
+
+
+class Arguments:
+    def __init__(self, level: int, threads: int):
+        # 'level' accepts any negative numbers but limits
+        # to the level 22 on the positive end.
+        self.level = min(level, 22)
+
+        # If provided 'threads' exceeds valid value
+        # (more than -1 and less than maximum available cpus)
+        # then apply correction.
+        if 0 <= threads <= cpu_count():
+            self.threads = threads
+        else:
+            self.threads = 0
