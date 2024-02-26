e55bb73b1fb80e53ec0374d0c7ff81f8d5aaa7ce
knighthat <knightchanelgaming@gmail.com>
2024-02-26 16:30:21 -0600

    added setup for module control

---
+++ setup.py
@@ -0,0 +1,46 @@
+from inspect import getsourcefile
+
+from setuptools import setup
+
+from src import __version__, __author__, __url__, __license__
+
+PROJ_HERE = getsourcefile(lambda: 0)
+
+setup(
+    name="ZSTD Backup",
+    version=__version__,
+    author=__author__,
+    author_email="",
+    description="Backup your files/folders using ZStandard algorithm",
+    url=__url__,
+    download_url=f'{__url__}/releases/latest',
+    py_modules=['zstd_backup'],
+    install_required=['PyYAML', 'zstandard'],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: System Administrators",
+        f"License :: OSI Approved :: {__license__}",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Operating System :: Microsoft :: Windows :: Windows 11",
+        "Operating System :: Unix",
+        "Operating System :: MacOS",
+        "Topic :: System :: Archiving",
+        "Topic :: System :: Archiving :: Backup",
+        "Topic :: System :: Archiving :: Compression",
+        "Topic :: Utilities"
+    ],
+    project_urls={
+        "Tracker": f"{__url__}/issues",
+        "Source": __url__,
+    },
+    python_requires=">=3.9",
+)
---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+__version__ = '0.0.1'
+__license__ = 'MIT License'
+__author__ = 'knighthat'
+__url__ = 'https://github.com/knighthat/zstd-backup'
