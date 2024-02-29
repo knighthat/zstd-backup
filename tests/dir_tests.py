f3a5d656b3a5f56f62a0a06eefc154ba105b4e59
knighthat <knightchanelgaming@gmail.com>
2024-02-28 23:11:10 -0600

    reworked on test cases

---
+++ tests/dir_tests.py
@@ -0,0 +1,114 @@
+import os
+import unittest
+from datetime import datetime, timedelta
+from shutil import rmtree
+
+from src import PROJECT_DIR, time_format, today
+from src.dir import abspath, ReturnCode, folder_exist, scan_4_backup
+from tests import TEST_DIR
+
+
+class FileAndFolderTest(unittest.TestCase):
+
+    def test_folder_exist(self):
+        path: str = os.path.join(TEST_DIR, 'folder1')
+
+        self.assertEqual(folder_exist(path), ReturnCode.NOT_EXIST, 'This folder is not supposed to exist!')
+
+        # Create an empty FILE named 'folder1'
+        with open(path, 'w'):
+            pass
+        self.assertEqual(folder_exist(path), ReturnCode.WRONG_TYPE, '\"folder1\" is supposed to be a file!')
+        os.remove(path)
+
+        # Create an empty DIRECTORY named 'folder1'
+        os.mkdir(path)
+        self.assertEqual(folder_exist(path), ReturnCode.EXIST, 'Directory \"folder1\" does not exist!')
+        rmtree(path)
+
+
+class BackupScannerTest(unittest.TestCase):
+    path: str = os.path.join(TEST_DIR, 'dates')
+    dates: set[str] = set()
+
+    @classmethod
+    def setUp(cls):
+        os.makedirs(cls.path, exist_ok=True)
+
+        # This for-loop will go from 45 to 0
+        # then it will subtract the date from today
+        # For example, i=2 will result in the
+        # date of 2 days before.
+        for i in range(45, -1, -1):
+            date: datetime = today - timedelta(days=i)
+
+            filename: str = f'{date.strftime(time_format)}.zstd'
+            filepath: str = os.path.join(cls.path, filename)
+            cls.dates.add(filepath)
+
+            with open(filepath, 'w'):
+                pass
+
+    @classmethod
+    def tearDown(cls):
+        rmtree(cls.path)
+
+    def test_scan_4_backup(self):
+        scanned: list[str] = scan_4_backup(self.path)
+        self.assertCountEqual(self.dates, scanned)
+
+
+class AbsPathTest(unittest.TestCase):
+
+    def test_absolute_path(self):
+        """
+        This scenario asserts the path will not be
+        converted because it's already in absolute form.
+        """
+        result: str = abspath('/path/to/file')
+        self.assertEqual(result, '/path/to/file')
+        self.assertTrue(os.path.isabs(result))
+
+    def test_dot_relative_path(self):
+        """
+        This scenario tests the conversion of '.' (current directory) character.
+        The '.' character must the converted into the project's directory
+        (main.py's dir) and the result must be an absolute path.
+        """
+        result: str = abspath('./rel/path')
+        self.assertEqual(result, f'{PROJECT_DIR}/rel/path')
+        self.assertTrue(os.path.isabs(result))
+
+    def test_dot_file_path(self):
+        """
+        This scenario will not convert '.' character because it's a file
+        starts with a dot (signal for hidden file on Linux machine).
+        This relative path will be appended to the project's directory
+        (main.py's dir) and the result must be an absolute path
+        """
+        result: str = abspath('.dotfile')
+        self.assertEqual(result, f'{PROJECT_DIR}/.dotfile')
+        self.assertTrue(os.path.isabs(result))
+
+    def test_user_home_path(self):
+        """
+        This scenario will test the conversion of '~' character.
+        The '~' character must be converted into the value of $HOME
+        and the result must be an absolute path.
+        """
+        result: str = abspath('~/directory')
+        self.assertEqual(result, f'{os.path.expanduser("~")}/directory')
+        self.assertTrue(os.path.isabs(result))
+
+    def test_other_cases(self):
+        """
+        This scenario will append project's dir (main.py's dir)
+        to the beginning of the path to make it absolute.
+        """
+        result: str = abspath(';/rel/path')
+        self.assertEqual(result, f'{PROJECT_DIR}/;/rel/path')
+        self.assertTrue(os.path.isabs(result))
+
+
+if __name__ == '__main__':
+    unittest.main()
