aff7bf35e7b5843697518973b8fbebfbe317fbec
knighthat <knightchanelgaming@gmail.com>
2024-02-28 23:12:49 -0600

    added new BackupProfile class

--- src/backup.py
+++ src/backup.py
@@ -2,7 +2,130 @@ import os
 from datetime import timedelta
 
 from src import today, time_format, PROJECT_DIR, dir, logger
+from src.config import Configuration
 from src.parser import parse_date
+
+
+class BackupProfile:
+    def __init__(self, config: Configuration):
+        #
+        #   Ignore paths
+        #
+        self.ignore: set[str] = set()
+        for path in config.ignore_paths:
+            abspath: str = dir.abspath(path)
+            self.ignore.add(abspath)
+
+        #
+        #   Destination path
+        #
+        self.destination = dir.abspath(config.destination)
+        if dir.folder_exist(self.destination) != dir.ReturnCode.EXIST:
+            os.makedirs(self.destination)
+        logger.debug(f'Backup will be saved to: {self.destination}')
+
+        #
+        #   Paths to compress
+        #
+        self._set_children(config.include)
+
+        #
+        #   Name of compressed file
+        #
+        self.filename = f'{today.strftime(time_format)}.zstd'
+
+    def _set_children(self, paths: set[str]) -> None:
+        """
+        Loop through provided paths, and add filtered
+        paths to 'self.children'
+        :param paths: to compress
+        """
+        self.children: set[str] = set()
+
+        def _add_file(path: str) -> None:
+            """
+            Add 'path' to 'self.children' if
+            path isn't listed in 'self.ignore_paths'
+            :param path: to compress
+            """
+            for ign in self.ignore:
+                if path.startswith(ign):
+                    logger.debug(f'Ignore {path}!')
+                else:
+                    self.children.add(path)
+
+        def _add_dir(path: str) -> None:
+            """
+            Loop through every single file inside this folder
+            and use '_add_file()' to add file's path to 'self.children'
+            :param path: to loop through
+            """
+            for root, dirs, files in os.walk(path):
+                for name in files:
+                    _add_file(os.path.join(root, name))
+
+        for p in paths:
+            " Convert path to absolute path "
+            abspath = dir.abspath(p)
+            if not os.path.exists(abspath):
+                " If path doesn't exist, skip next step "
+                logger.warn(f'{abspath} does not exist! Skipping...')
+                continue
+
+            " Path can be either folder or file, use appropriate function for each"
+            if os.path.isdir(abspath):
+                _add_dir(abspath)
+            elif os.path.isfile(abspath):
+                _add_file(abspath)
+
+    def __len__(self) -> int:
+        """
+        Calculate the size (in bytes)
+        of the total files add up.
+        :return: total size of children in bytes
+        """
+
+        def _size(abspath: str) -> int:
+            """
+            :param abspath: path to file or folder
+            :return: size in bytes
+            """
+            try:
+                return os.path.getsize(abspath)
+            except FileNotFoundError:
+                logger.warn(f'Error while reading {abspath}! Could be a broken link (shortcut). Skipping...')
+            except Exception as e:
+                logger.warn(f'Error while calculating size of {abspath}! Skipping...')
+                logger.exception(e)
+
+            return 0
+
+        def _dir_size(abspath: str) -> int:
+            """
+            Calculate directory's size by adding each file
+            inside the folder (including files inside sub-folder).
+            :param abspath: path to folder
+            :return: size in bytes
+            """
+            total: int = 0
+            for root, dirs, names in os.walk(abspath):
+                for name in names:
+                    filepath = os.path.join(root, name)
+                    total += _size(filepath)
+
+            return total
+
+        size: int = 0
+        for file in self.children:
+            if os.path.isfile(file):
+                size += _size(file)
+            elif os.path.isdir(file):
+                size += _dir_size(file)
+
+        return size
+
+    def __str__(self) -> str:
+        return f'BackupProfile(name={self.filename}, size={len(self)})'
 
 
 class Backup:
