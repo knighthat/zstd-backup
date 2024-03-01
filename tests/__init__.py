6c6e24166a3a4b261a1a60ebe152bc55bb303cce
knighthat <knightchanelgaming@gmail.com>
2024-02-29 23:26:17 -0600

    separated test paths for windows

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,19 +1,13 @@
-from os import makedirs
 from os.path import join
+from platform import system
 
 from src import PROJECT_DIR
 
 valid_config: dict = {
     'console_log_level': 'DEBUG',
-    'include': [
-        './tests/include/1',
-        './tests/include/2',
-        './tests/include/3',
-        './tests/include/4',
-        './tests/include/ignore'
-    ],
-    'destination': './tests/backups',
-    'ignore': './tests/include/ignore',
+    'include': [],
+    'destination': '',
+    'ignore': '',
     'old_backups': {
         'keep': 1,
         'retention': 2,
@@ -27,3 +21,25 @@ valid_config: dict = {
 }
 
 TEST_DIR: str = join(PROJECT_DIR, 'tests')
+
+# Apply different path type depends on system
+if system() == 'Windows':
+    valid_config['include'] = [
+        '.\\tests\\include\\1',
+        '.\\tests\\include\\2',
+        '.\\tests\\include\\3',
+        '.\\tests\\include\\4',
+        '.\\tests\\include\\ignore'
+    ]
+    valid_config['destination'] = '.\\tests\\backups'
+    valid_config['ignore'] = '.\\tests\\include\\ignore'
+else:
+    valid_config['include'] = [
+        './tests/include/1',
+        './tests/include/2',
+        './tests/include/3',
+        './tests/include/4',
+        './tests/include/ignore'
+    ]
+    valid_config['destination'] = './tests/backups'
+    valid_config['ignore'] = './tests/include/ignore'
